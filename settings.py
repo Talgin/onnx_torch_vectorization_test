64ab7c169c2415b082ae8937746c2c171bd2d2f1
Talgin <talgat90.07@gmail.com>
2024-01-08 03:39:13 +0600

    changed to track large files

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
+
+DET_MODEL_FOLDER = './models/detection/'
+REC_MODEL_FOLDER ='./models/recognition/'
+
+IMAGES_ROOT = '/umai_home/Akati/data/'
+
+FAISS_GBDFL_FILE = '/TENGRI/STORAGE/final_index/populated.index'
